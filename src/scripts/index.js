a185be1b690c5ca88320ef882ff282c774d28c08
Amopho <amopho78@gmail.com>
2021-05-12 01:08:01 +0200

    function genie and viewbox with copy

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,43 @@
+// Clear the concole on every refresh
+console.clear();
+
+// object with appropiate functions according to their features
+// const randomFunc = {
+// 	lower: getRandomLower,
+// 	upper: getRandomUpper,
+// 	number: getRandomNumber,
+// 	symbol: getRandomSymbol,
+// };
+
+//selecting DOM elements
+// The Viewbox where the result will be shown
+const resultEl = document.getElementById("result");
+
+// // checkbobes for respective options responsible to generate varies type of password
+// const uppercaseEl = document.getElementById("uppercase");
+// const lowercaseEl = document.getElementById("lowercase");
+// const numberEl = document.getElementById("number");
+// const symbolEl = document.getElementById("symbol");
+
+// Button to generate the password
+const generateBtn = document.getElementById("generate");
+
+// Result viewbox container
+const resultContainer = document.querySelector(".result");
+
+// Button to copy the text
+const copyBtn = document.getElementById("copy-btn");
+
+// When Generate is clicked Password id generated.
+generateBtn.addEventListener("click", () => {
+  generatedPassword = true;
+  resultEl.innerText = generatePassword();
+});
+
+// Function responsible to generate password and then returning it.
+//function that generates random 30digits password
 function generatePassword() {
-  let inputPass = "";
+  let generatedPassword = "";
   let availableCharacters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
     "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
@@ -7,21 +45,31 @@ function generatePassword() {
   for (i = 1; i <= 30; i++) {
     let char = Math.floor(Math.random() * availableCharacters.length + 1);
 
-    inputPass += availableCharacters.charAt(char);
+    generatedPassword += availableCharacters.charAt(char);
   }
 
-  return inputPass;
+  return generatedPassword;
 }
 
 console.log("Your new password: " + generatePassword());
 
-const copy = () => {
-  let text = document.querySelector(".userInput");
-  text.select();
+//copy to clipboard
+// const copy = () => {
+//   let text = document.querySelector(".userInput");
+//   text.select();
+//   document.execCommand("copy");
+// };
+// document.querySelector(".copyButton").addEventListener("click", copy);
+
+copyBtn.addEventListener("click", () => {
+  const textarea = document.createElement("textarea");
+  const password = resultEl.innerText;
+  if (!password || password == "click generate") {
+    return;
+  }
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
   document.execCommand("copy");
-};
-document.querySelector(".copyButton").addEventListener("click", copy);
-
-// form {
-
-// }
+  textarea.remove();
+});
